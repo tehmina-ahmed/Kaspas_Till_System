ab3f0a15f29b4306f943df145232e375b90defbd
tehmina-ahmed <82107262+tehmina-ahmed@users.noreply.github.com>
2021-05-10 09:00:07 +0100

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,231 @@
+﻿using KaspasApp.Data;
+using KaspasApp.Models;
+using KaspasApp.Models.Binding;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace KaspasApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductsController(ApplicationDbContext applicationDbContext)
+        {
+            dbContext = applicationDbContext;
+        }
+        #region
+        //Reads the View 
+        //public IActionResult Index()
+        //{
+        //    var allProducts = dbContext.Products.ToList();
+        //    return View(allProducts);
+        //}
+        //[Route("details/{int:id}")]
+        //public IActionResult Details(int id)
+        //{
+        //    var productById = dbContext.Products.FirstOrDefault(p => p.ID == id);
+        //    return View(productById);
+        //}
+
+        ////CREATEs the actions 
+        //[Route("create")]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+        //[HttpPost("create")]
+        //public IActionResult Create(AddProductBindingModel bindingModel)
+        //{
+        //    var productToCreate = new Product
+        //    {
+        //        Name = bindingModel.Name,
+        //        Description = bindingModel.Description,
+        //        Price = bindingModel.Price,
+        //        PictureURL = bindingModel.PictureURL,
+        //        CreatedAt = DateTime.Now
+        //    };
+        //    dbContext.Products.Add(productToCreate);
+        //    dbContext.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        ////UPDATE
+        //[Route("update/{id:int}")]
+        //public IActionResult Update(int id)
+        //{
+        //    var productById = dbContext.Products.FirstOrDefault(p => p.ID == id);
+        //    return View(productById);
+        //}
+        //[HttpPost]
+        //[Route("update/{id:int}")]
+        //public IActionResult Update(Product products, int id)
+        //{
+        //    var productToUpdate = dbContext.Products.FirstOrDefault(p => p.ID == id);
+        //    productToUpdate.Name = products.Name;
+        //    productToUpdate.Description = products.Description;
+        //    productToUpdate.Price = products.Price;
+        //    productToUpdate.PictureURL = products.PictureURL;
+        //    dbContext.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        ////DELETE
+        //[Route("delete/{id:int}")]
+        //public IActionResult Delete(int id)
+        //{
+        //    var productToDelete = dbContext.Products.FirstOrDefault(p => p.ID == id);
+        //    dbContext.Products.Remove(productToDelete);
+        //    dbContext.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+#endregion
+        #region Toppings
+        //Toppings Section
+        //CREATE
+        [Route("addtopping/{productID:int}")]
+        public IActionResult CreateToppings(int productID)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.ID == productID);
+            ViewBag.ProductName = product.Name;
+            return View();
+        }
+        [HttpPost]
+        [Route("addtopping/{productID:int}")]
+        public IActionResult CreateToppings(AddToppingBindingModel bindingModel, int productID)
+        {
+            bindingModel.ProductID = productID;
+            var toppingsToCreate = new Topping
+            {
+                Name = bindingModel.Name,
+                Description = bindingModel.Description,
+                
+                //ProductID = dbContext.Products.FirstOrDefault(p => p.ID == productID),
+                Product = dbContext.Products.FirstOrDefault(p => p.ID == productID),
+
+                PictureURL = bindingModel.PictureURL
+                
+            };
+            dbContext.Toppings.Add(toppingsToCreate);
+            dbContext.SaveChanges();
+            //return RedirectToAction("Index");
+            return RedirectToAction("Index", new { id = productID });
+        }
+        [Route("{id:int}/toppings")]
+        public IActionResult ViewToppings(int id)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            var toppings = dbContext.Toppings.Where(p => p.ProductID == id).ToList();
+            ViewBag.ProductName = product.Name;
+            return View(toppings);
+
+        }
+        #endregion
+        #region Product
+        //READ
+        [Route("")]
+        public IActionResult ProductIndex()
+        {
+            var allProducts = dbContext.Products.ToList();
+            return View(allProducts);
+        }
+        [Route("details/{id:int}")]
+        public IActionResult ProductDetails(int id)
+        {
+            var productById = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            return View(productById);
+        }
+        //CREATE
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("create")]
+        public IActionResult Create(AddProductBindingModel bindingModel)
+        {
+            var productToCreate = new Models.Product
+            {
+                Name = bindingModel.Name,
+                Description = bindingModel.Description,
+                Price = bindingModel.Price,
+
+                PictureURL = bindingModel.PictureURL,
+                CreatedAt = DateTime.Now
+            };
+            dbContext.Products.Add(productToCreate);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //UPDATE
+        [Route("update/{id:int}")]
+        public IActionResult Update(int id)
+        {
+            var productById = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            return View(productById);
+        }
+        [HttpPost]
+        [Route("update/{id:int}")]
+        public IActionResult Update(Models.Product product, int id)
+        {
+            var productToUpdate = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            productToUpdate.Name = product.Name;
+            productToUpdate.Description = product.Description;
+            productToUpdate.PictureURL = product.PictureURL;
+            productToUpdate.Price = product.Price;
+            
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //DELETE
+        [Route("delete/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var productToDelete = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            dbContext.Products.Remove(productToDelete);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+        #region Topping
+        //Topping Section
+        //CREATE
+        [Route("addtopping/{productID:int}")]
+        public IActionResult CreateTopping(int productID)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.ID == productID);
+            ViewBag.ProductName = product.Name;
+            return View();
+        }
+        [HttpPost]
+        [Route("addtopping/{productID:int}")]
+        public IActionResult CreateTopping(AddToppingBindingModel bindingModel, int productID)
+        {
+            bindingModel.ProductID = productID;
+            var toppingToCreate = new Topping//had to add s
+            {
+                Name = bindingModel.Name,
+                Description = bindingModel.Description,
+                
+                Product = dbContext.Products.FirstOrDefault(p => p.ID == productID),
+
+                PictureURL = bindingModel.PictureURL,
+                CreatedAt = DateTime.Now,
+            };
+            dbContext.Toppings.Add(toppingToCreate);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Route("{id:int}/toppings")]
+        public IActionResult ViewProductToppings(int id)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.ID == id);
+            var topping = dbContext.Toppings.Where(p => p.Product.ID == id).ToList();
+            ViewBag.ProductName = product.Name;
+            return View(topping);
+        }
+        #endregion
+    }
+}
---
+++ Controllers/ToppingsController.cs
@@ -0,0 +1,72 @@
+﻿using KaspasApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaspasApp.Controllers
+{
+    [Route("[Controller]")]
+    public class ToppingsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public DateTime CreatedAt { get; internal set; }
+        public string PictureURL { get; internal set; }
+        public Models.Product Product { get; internal set; }
+        public string Description { get; internal set; }
+        public string Name { get; internal set; }
+
+        public ToppingsController(ApplicationDbContext applicationDbContext)
+        {
+            dbContext = applicationDbContext;
+        }
+        //READ
+        [Route("")]
+        public IActionResult Index()
+        {
+            var alltoppings = dbContext.Toppings.ToList();
+            return View(alltoppings);
+        }
+        [Route("details/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            var toppingsById = dbContext.Toppings.FirstOrDefault(t => t.ID == id);
+            return View(toppingsById);
+        }
+
+        //UPDATE
+        [Route("update/{id:int}")]
+        public IActionResult Update(int id)
+        {
+            var toppingsById = dbContext.Toppings.FirstOrDefault(p => p.ID == id);
+            return View(toppingsById);
+        }
+        [HttpPost]
+        [Route("update/{id:int}")]
+        public IActionResult Update(ToppingsController topping, int id)
+        {
+            var toppingsToUpdate = dbContext.Toppings.FirstOrDefault(p => p.ID == id);
+            toppingsToUpdate.Name = topping.Name;
+            toppingsToUpdate.Description = topping.Description;
+            toppingsToUpdate.PictureURL = topping.PictureURL;
+            
+            
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //DELETE
+        [Route("delete/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var toppingsToDelete = dbContext.Toppings.FirstOrDefault(p => p.ID == id);
+            dbContext.Toppings.Remove(toppingsToDelete);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+       
+
